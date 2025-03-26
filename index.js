25e1c4c7f31f4ea7b86934110ff543cef3c4771f
brido-PGM <brido0002@gmail.com>
2025-03-26 18:06:33 -0500

    Correccion de config e index

--- index.js
+++ index.js
@@ -8,10 +8,7 @@ app.use(exp.json());    //  todo lo que se manda o se recibe se visualiza como u
 app.use(exp.urlencoded({extended:false}));  //  las url pueden tener datos codificados
 
 app.use('/v1', enrutador);
-app.use('/v2', enrutador);
-// app.use('/v3', enrutadorCreacion);
 
-
-app.listen(9999, ()=>{
-    console.log('Estamos en el puerto' + 9999);
-})
+app.listen(process.env.PORT, ()=>{
+    console.log('Estamos en el puerto' + process.env.PORT);
+});
--- src/config/conection.js
+++ src/config/conection.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-//const URI = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@adso2903013.o31ha.mongodb.net/${process.env.BD}`
-const URI =  `mongodb+srv://brianAdso:2UvdkRtg3ZnZQVkk@adso2903013.o31ha.mongodb.net/APITienda`
+const URI =  `mongodb+srv://${process.env.USER}:${process.env.CON}@adso2903013.o31ha.mongodb.net/${process.env.DB}`
 mongoose.connect(URI)
 
 module.exports = mongoose;
